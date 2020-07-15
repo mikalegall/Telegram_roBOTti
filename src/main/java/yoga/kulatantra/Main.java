05e9d7b0af6050d17293e9cdff66987c9e643ec3
Mika Le Gall <mika.legall@live.fi>
2020-07-15 11:27:13 +0300

    buildpack test3

--- src/main/java/yoga/kulatantra/Main.java
+++ src/main/java/yoga/kulatantra/Main.java
@@ -11,7 +11,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 // heroku login
 // Google credentials on json format into Heroku enviroment variable
-// https://elements.heroku.com/buildpacks/buyersight/heroku-google-application-credentials-buildpack
+// https://elements.heroku.com/search/buildpacks?q=GOOGLE_APPLICATION_CREDENTIALS
 
 // heroku logs --app stagingshalarelax --source app --tail
 // heroku run bash --app stagingshalarelax
